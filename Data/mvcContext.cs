e0fdb883822965059dd6d1e81a91529f358c580f
Bruno Nicolau Machado <nicolau.cortella@gmail.com>
2019-06-19 22:13:50 -0300

    Create Entites and DB

--- Data/mvcContext.cs
+++ Data/mvcContext.cs
@@ -13,6 +13,9 @@ namespace mvc.Models
         {
         }
 
-        public DbSet<mvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
