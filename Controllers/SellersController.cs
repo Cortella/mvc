a60d998d5d763f190f8c3d9c20b155ab78b4b2eb
Bruno Nicolau Machado <nicolau.cortella@gmail.com>
2019-06-22 05:05:47 -0300

    Ends Form "Problema save DB"

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
 using mvc.Services;
 
 namespace mvc.Controllers
@@ -10,16 +11,34 @@ namespace mvc.Controllers
     public class SellersController : Controller
     {
 
-        private readonly SellerService _sellerservice;
+        private readonly SellerService _sellerService;
 
         public SellersController(SellerService sellerService)
         {
-            _sellerservice = sellerService;
+            _sellerService = sellerService;
         }
         public IActionResult Index()
         {
-            var list = _sellerservice.FindAll();
+            var list = _sellerService.FindAll();
             return View(list);
+        }
+      //  public IActionResult Crete()
+      //  {
+      //      return View();
+      //  }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //Anotation -> indica que e acao de post e nao de get
+        [ValidateAntiForgeryToken] //Previne ataques CSRF (Envio de dados maliciosos)
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index)); //Melhora manutencao do sistema, pois caso string Index mude, nao precisa corrigir aqui
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using mvc.Models;
 using mvc.Services;
+using mvc.Data;
 
 namespace mvc
 {
@@ -41,15 +42,17 @@ namespace mvc
                     options.UseMySql(Configuration.GetConnectionString("mvcContext"), builder => 
                     builder.MigrationsAssembly("mvc")));
 
+            services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
